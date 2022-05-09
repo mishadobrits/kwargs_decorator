34a817fd9ed59997dd6ef32aaa0da03795882b5b
MishaD2003 <MishaBelka2003@yandex.ru>
2022-05-09 13:05:12 +0300

    Setup test

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="Args and kwargs decorator",
+    name="kwargs_decorator",
     description="Provides more flexible usage of the args and kwargs in python functions",
     version="1.0.0",
 )
